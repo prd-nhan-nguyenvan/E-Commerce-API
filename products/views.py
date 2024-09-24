baa0ece495e8a2318023ef5ba0b9b8405076d085
Nhan Nguyen Van Khanh <nhan.nguyenvan@paradox.ai>
2024-09-24 16:51:04 +0700

    feat(swagger): add folder for API

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
@@ -9,10 +10,34 @@ class CategoryListCreateView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+    @swagger_auto_schema(tags=["Categories"])
+    def get(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=["Categories"])
+    def post(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
 
 class CategoryRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+    @swagger_auto_schema(tags=["Categories"])
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=["Categories"])
+    def put(self, request, *args, **kwargs):
+        return super().put(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=["Categories"])
+    def patch(self, request, *args, **kwargs):
+        return super().patch(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=["Categories"])
+    def delete(self, request, *args, **kwargs):
+        return super().delete(request, *args, **kwargs)
 
 
 class CategoryRetrieveBySlugView(generics.RetrieveAPIView):
@@ -20,21 +45,53 @@ class CategoryRetrieveBySlugView(generics.RetrieveAPIView):
     serializer_class = CategorySerializer
     lookup_field = "slug"
 
+    @swagger_auto_schema(tags=["Categories"])
+    def get(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
 
 class ProductListCreateView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    @swagger_auto_schema(tags=["Products"])
+    def get(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=["Products"])
+    def post(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
 
 
 class ProductRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+    @swagger_auto_schema(tags=["Products"])
+    def get(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=["Products"])
+    def put(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=["Products"])
+    def patch(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=["Products"])
+    def delete(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
+
 
 class ProductRetrieveBySlugView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = "slug"
+
+    @swagger_auto_schema(tags=["Products"])
+    def get(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
 
 
 class ReviewCreateView(generics.CreateAPIView):
@@ -44,6 +101,10 @@ class ReviewCreateView(generics.CreateAPIView):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)  # Set the user from the request
 
+    @swagger_auto_schema(tags=["Review"])
+    def post(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
 
 class ProductReviewListView(generics.ListAPIView):
     serializer_class = ReviewSerializer
@@ -51,3 +112,7 @@ class ProductReviewListView(generics.ListAPIView):
     def get_queryset(self):
         product_id = self.kwargs["product_id"]
         return Review.objects.filter(product_id=product_id)
+
+    @swagger_auto_schema(tags=["Review"])
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)

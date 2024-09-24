8cdc359906eb3f20d633f4d33a3e4f961bfd3a20
Nhan Nguyen Van Khanh <nhan.nguyenvan@paradox.ai>
2024-09-24 14:09:45 +0700

    feat(IL-1004): add CRUD end point for Category model

--- products/views.py
+++ products/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from rest_framework import generics
+
+from .models import Category
+from .serializers import CategorySerializer
+
+
+class CategoryListCreateView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CategoryRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer

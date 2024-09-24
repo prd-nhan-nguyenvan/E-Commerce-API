077fb033f689c00a6c9c7eb8369211ca9ce9bb4c
Nhan Nguyen Van Khanh <nhan.nguyenvan@paradox.ai>
2024-09-24 13:43:35 +0700

    feat(IL-1004): add Category model

--- products/models.py
+++ products/models.py
@@ -1,1 +1,9 @@
-# Create your models here.
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name

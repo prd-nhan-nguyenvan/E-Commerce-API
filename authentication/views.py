470732ec0d08f69147b0123d0ff8f9fb131dfef4
Nhan Nguyen Van Khanh <nhan.nguyenvan@paradox.ai>
2024-09-24 10:44:09 +0700

    feat(IT-1002): add change password end point

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,8 @@
-from rest_framework import status
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import RegisterSerializer
+from .serializers import ChangePasswordSerializer, RegisterSerializer
 
 
 class RegisterView(APIView):
@@ -14,3 +14,19 @@ class RegisterView(APIView):
                 {"message": "User created successfully"}, status=status.HTTP_201_CREATED
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ChangePasswordView(generics.UpdateAPIView):
+    serializer_class = ChangePasswordSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+    def put(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(
+            {"message": "Password updated successfully"}, status=status.HTTP_200_OK
+        )

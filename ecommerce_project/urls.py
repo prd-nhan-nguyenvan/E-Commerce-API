bebe7ea4fafb7b4e4b25343dc19ada784097c6d0
Nhan Nguyen Van Khanh <nhan.nguyenvan@paradox.ai>
2024-09-25 08:35:52 +0700

    feat(users): create end point for detail profile and edit profile

--- ecommerce_project/urls.py
+++ ecommerce_project/urls.py
@@ -20,6 +20,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("auth/", include("authentication.urls")),
     path("api/products/", include("products.urls")),
+    path("api/users/", include("users.urls")),
     path(
         "swagger/",
         schema_view.with_ui("swagger", cache_timeout=0),
--- users/views.py
+++ users/views.py
@@ -1,1 +1,21 @@
-# Create your views here.
+from rest_framework import generics, permissions
+
+from .models import UserProfile
+from .serializers import UserProfileSerializer
+
+
+class ProfileRetrieveUpdateView(generics.RetrieveUpdateAPIView):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return UserProfile.objects.get(user=self.request.user)
+
+    def put(self, request, *args, **kwargs):
+        # For a full update, ensure the user field remains unchanged
+        return self.partial_update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        # Ensure user cannot update the `user` field directly
+        return super().partial_update(request, *args, **kwargs)

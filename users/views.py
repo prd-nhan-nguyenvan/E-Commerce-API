5432156380042f58804ed96378d5da53668baf17
Nhan Nguyen Van Khanh <nhan.nguyenvan@paradox.ai>
2024-09-25 08:56:30 +0700

    feat(Swagger): add User folder in swagger

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import generics, permissions
 
 from .models import UserProfile
@@ -12,10 +13,16 @@ class ProfileRetrieveUpdateView(generics.RetrieveUpdateAPIView):
     def get_object(self):
         return UserProfile.objects.get(user=self.request.user)
 
+    @swagger_auto_schema(tags=["User"])
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=["User"])
     def put(self, request, *args, **kwargs):
         # For a full update, ensure the user field remains unchanged
         return self.partial_update(request, *args, **kwargs)
 
+    @swagger_auto_schema(tags=["User"])
     def patch(self, request, *args, **kwargs):
         # Ensure user cannot update the `user` field directly
         return super().partial_update(request, *args, **kwargs)

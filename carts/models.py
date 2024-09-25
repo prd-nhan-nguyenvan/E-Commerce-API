59a8ef689e18f24e0963ff1e3df38f2ae72472be
Nhan Nguyen Van Khanh <nhan.nguyenvan@paradox.ai>
2024-09-25 09:10:10 +0700

    feat(carts): create Cart and CartItem model

--- carts/models.py
+++ carts/models.py
@@ -1,1 +1,25 @@
-# Create your models here.
+from django.conf import settings
+from django.db import models
+
+from products.models import Product
+
+
+class Cart(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Cart of {self.user.email}"
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    class Meta:
+        unique_together = ["cart", "product"]
+
+    def __str__(self):
+        return (
+            f"{self.quantity} of {self.product.name} in {self.cart.user.email}'s cart"
+        )

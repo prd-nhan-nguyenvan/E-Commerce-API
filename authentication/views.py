c851766d967af1aaa2ead8edc57bbf9f5fc4eb2d
Nhan Nguyen Van Khanh <nhan.nguyenvan@paradox.ai>
2024-09-30 16:27:52 +0700

    refactor(login): handle error in refresh token

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,5 @@
+from tokenize import TokenError
+
 from django.contrib.auth import authenticate
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import generics, permissions, status
@@ -53,7 +55,6 @@ class CustomTokenRefreshView(APIView):
         serializer = RefreshTokenSerializer(data=request.data)
         if serializer.is_valid():
             refresh_token = serializer.validated_data.get("refresh")
-
             if not refresh_token:
                 return Response(
                     {"detail": "Refresh token is required."},
@@ -61,12 +62,8 @@ class CustomTokenRefreshView(APIView):
                 )
 
             try:
-                # Create a RefreshToken instance using the provided refresh token
                 refresh = RefreshToken(refresh_token)
-
-                # Get the new access token from the refresh token
                 new_access_token = refresh.access_token
-
                 return Response(
                     {
                         "access": str(new_access_token),
@@ -74,9 +71,11 @@ class CustomTokenRefreshView(APIView):
                     },
                     status=status.HTTP_200_OK,
                 )
-            except Exception:
+            except TokenError as e:
                 return Response(
-                    {"detail": "Invalid refresh token."},
+                    {
+                        "detail": str(e)
+                    },  # This will provide the specific error from the token library
                     status=status.HTTP_400_BAD_REQUEST,
                 )
 

caff61afdaed1d97b3e5d3cb7dfd8230123917c0
Nhan Nguyen Van Khanh <nhan.nguyenvan@paradox.ai>
2024-09-30 16:44:54 +0700

    feat(permission): add role to User Model to management role and permissions

--- authentication/models.py
+++ authentication/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import (
 )
 from django.db import models
 
+from .constants import ROLE_ADMIN, ROLE_STAFF, ROLE_USER
+
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
@@ -26,8 +28,16 @@ class CustomUserManager(BaseUserManager):
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=100, unique=True)
+    role = models.CharField(
+        max_length=20,
+        choices=[
+            (ROLE_ADMIN, "Admin"),
+            (ROLE_USER, "User"),
+            (ROLE_STAFF, "Staff"),
+        ],
+        default=ROLE_USER,
+    )
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
 
     objects = CustomUserManager()

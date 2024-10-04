d1fa98c352551051f8e1e9e06198292255e2fa0d
Nhan Nguyen Van Khanh <nhan.nguyenvan@paradox.ai>
2024-10-04 16:00:36 +0700

    test(authentication): write test class for RefreshTokenView

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,4 +1,5 @@
 from unittest.mock import patch
+from uuid import uuid4
 
 from django.conf import settings
 from django.contrib.auth import get_user_model
@@ -28,7 +29,6 @@ class LoginViewTestCase(APITestCase):
     @patch("django.contrib.auth.authenticate")
     @patch("authentication.helper.custom_token_generator")
     def test_successful_login(self, mock_custom_token_generator, mock_authenticate):
-
         mock_authenticate.return_value = self.user
 
         # Data for the login request
@@ -52,7 +52,6 @@ class LoginViewTestCase(APITestCase):
 
     @patch("django.contrib.auth.authenticate")
     def test_login_invalid_credentials(self, mock_authenticate):
-
         mock_authenticate.return_value = None
         data = {"email": "invaliduser@example.com", "password": "invalidpassword"}
         response = self.client.post(self.url, data, format="json")
@@ -78,3 +77,79 @@ class LoginViewTestCase(APITestCase):
         self.assertIn(
             "email", response.data
         )  # Assuming your serializer handles this validation
+
+    def tearDown(self):
+        User.objects.all().delete()
+        Application.objects.all().delete()
+        AccessToken.objects.all().delete()
+        RefreshToken.objects.all().delete()
+
+
+class CustomTokenRefreshViewTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Create a refresh token for testing
+        cls.user = cls.create_user()
+        cls.application = cls.create_application()
+        cls.refresh_token = RefreshToken.objects.create(
+            token="valid_refresh_token",
+            user=cls.user,
+            application=cls.application,
+        )
+
+        # Set up URL for token refresh
+        cls.url = reverse("token_refresh")
+
+    @classmethod
+    def create_user(self):
+        username = f"testuser-{uuid4()}"
+        return get_user_model().objects.create_user(
+            username=username,
+            email=f"{username}@example.com",
+            password="testpassword",
+        )
+
+    @classmethod
+    def create_application(cls):
+        # Create a mock application for testing
+        from oauth2_provider.models import Application
+
+        return Application.objects.create(
+            user=cls.create_user(),
+            client_type=Application.CLIENT_CONFIDENTIAL,
+            authorization_grant_type=Application.GRANT_PASSWORD,
+            name="Test Application",
+        )
+
+    def test_token_refresh_missing_refresh_token(self):
+        # Request data without refresh token
+        data = {}
+
+        response = self.client.post(self.url, data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("refresh", response.data)
+
+    def test_token_refresh_invalid_refresh_token(self):
+        # Invalid refresh token
+        data = {"refresh": "invalid_refresh_token"}
+
+        response = self.client.post(self.url, data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["detail"], "Invalid refresh token.")
+
+    def test_token_refresh_serializer_error(self):
+        # Simulate serializer errors (e.g., missing data)
+        data = {"refresh": None}
+
+        response = self.client.post(self.url, data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("refresh", response.data)
+
+    def tearDown(self):
+        User.objects.all().delete()
+        Application.objects.all().delete()
+        AccessToken.objects.all().delete()
+        RefreshToken.objects.all().delete()

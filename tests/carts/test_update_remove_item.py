bf38d697bca5344d1819f6a1947efae52c445299
Nhan Nguyen Van Khanh <nhan.nguyenvan@paradox.ai>
2024-11-05 14:03:53 +0700

    test(carts): add test delete when qualitity

--- tests/carts/test_update_remove_item.py
+++ tests/carts/test_update_remove_item.py
@@ -6,9 +6,19 @@ from rest_framework import status
 @pytest.mark.django_db
 def test_update_item_from_cart_success(api_client, user, product, add_product_to_cart):
     api_client.force_authenticate(user=user)
-    data = {"quantity": 2}
+    data = {"quantity": 5}
     response = api_client.patch(add_product_to_cart, data, format="json")
     assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_update_item_from_cart_remove_success(
+    api_client, user, product, add_product_to_cart
+):
+    api_client.force_authenticate(user=user)
+    data = {"quantity": 0}
+    response = api_client.patch(add_product_to_cart, data, format="json")
+    assert response.status_code == status.HTTP_204_NO_CONTENT
 
 
 @pytest.mark.django_db
@@ -24,3 +34,10 @@ def test_remove_item_from_cart_failure(api_client, user, product):
     invalid_url = reverse("update-remove-from-cart", args=[product.id + 1])
     response = api_client.delete(invalid_url)
     assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.django_db
+def test_empty_cart_success(api_client, user, product, add_product_to_cart):
+    api_client.force_authenticate(user=user)
+    response = api_client.delete(reverse("empty-cart"))
+    assert response.status_code == status.HTTP_204_NO_CONTENT
